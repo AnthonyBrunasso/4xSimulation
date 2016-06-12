b438b93fcf9aa7532253df263a9d8d1ac520fbe2
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2016-06-12 08:31:20 -0700

    UNKOWN is added to all game types.
    * type_generator no longer redirects stdout to file :)

--- data/game_types.py
+++ data/game_types.py
@@ -1,5 +1,8 @@
 from store import add_gametype 
 
+# NOTE: All game types will generate an UNKNOWN enum at id 0 so that
+# keyword and id is reserved, (unknown, 0)
+
 RESOURCE     = "RESOURCE"
 IMPROVEMENT  = "IMPROVEMENT"
 TERRAIN      = "TERRAIN"
@@ -8,13 +11,11 @@ BUILDING     = "BUILDING"
 CONSTRUCTION = "CONSTRUCTION"
 
 # Resources to generate when running type_generator.py
-add_gametype(RESOURCE, 'UNKNOWN'  , 0) # TODO: Get rid of this
 add_gametype(RESOURCE, 'Gold'     , 1)
 add_gametype(RESOURCE, 'Happiness', 2)
 add_gametype(RESOURCE, 'Sugar'    , 3)
 add_gametype(RESOURCE, 'Stone'    , 4)
 
-add_gametype(IMPROVEMENT, 'Unknown', 0)
 add_gametype(IMPROVEMENT, 'Resource', 1)
 
 add_gametype(TERRAIN, 'Desert'   , ord('d') - ord('a'))
@@ -30,10 +31,10 @@ add_gametype(UNIT, 'Phalanx', 3)
 
 add_gametype(BUILDING, 'Town', 1)
 
-add_gametype(CONSTRUCTION, 'Scout Unit', 0)
-add_gametype(CONSTRUCTION, 'Granary'   , 1)
-add_gametype(CONSTRUCTION, 'Range Unit', 2)
-add_gametype(CONSTRUCTION, 'Forge'     , 3)
-add_gametype(CONSTRUCTION, 'Melee Unit', 4)
-add_gametype(CONSTRUCTION, 'Uber Forge', 5)
-add_gametype(CONSTRUCTION, 'Invalid'   , 6)
+add_gametype(CONSTRUCTION, 'Scout Unit', 1)
+add_gametype(CONSTRUCTION, 'Granary'   , 2)
+add_gametype(CONSTRUCTION, 'Range Unit', 3)
+add_gametype(CONSTRUCTION, 'Forge'     , 4)
+add_gametype(CONSTRUCTION, 'Melee Unit', 5)
+add_gametype(CONSTRUCTION, 'Uber Forge', 6)
+add_gametype(CONSTRUCTION, 'Invalid'   , 7)
--- data/store.py
+++ data/store.py
@@ -1,6 +1,12 @@
+import sys
+
 game_types = {}
 
 def add_gametype(type_name, object_name, type_id):
+  if type_id == 0:
+    sys.stderr.write("0 is an invalid type_id for " + object_name + ", it is reserved for UNKNOWN.\n")
+    sys.exit()
+
   if type_name not in game_types:
     game_types[type_name] = []
 
