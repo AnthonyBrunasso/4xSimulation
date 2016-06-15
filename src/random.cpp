3cb24cc886f3e24ca9eda97196e54134504fb67d
Alan Newton <alan@rufe.org>
2016-06-14 17:50:49 -0700

    bug fix to random

--- src/random.cpp
+++ src/random.cpp
@@ -5,8 +5,11 @@
 
 sf::Vector3i game_random::cube_coord(int max_coord) { 
   int x = (rand() % (2 * max_coord)) - max_coord;
-  int t = x == 0 ? 1 : x;
-  int y = (rand() % (2 * t)) - t; 
+  int y = 0;
+  int t = (max_coord - abs(x));
+  if (t) {
+    y = (rand() % (2 * t)) - t; 
+  }
   int z = -x - y;
   return sf::Vector3i(x, y, z);
 }
