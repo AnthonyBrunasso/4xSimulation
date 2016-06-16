d7cfc2bc66ee265750c1c0605ecc65b459cd6900
Alan Newton <alan@rufe.org>
2016-06-15 21:19:33 -0700

    oops

--- src/random.cpp
+++ src/random.cpp
@@ -8,11 +8,11 @@ namespace game_random {
 }
 
 sf::Vector3i game_random::cube_coord(int max_coord) { 
-  int x = (rand() % (2 * max_coord)) - max_coord;
+  int x = (s_mt() % (2 * max_coord)) - max_coord;
   int y = 0;
   int t = (max_coord - abs(x));
   if (t) {
-    y = (rand() % (2 * t)) - t; 
+    y = (s_mt() % (2 * t)) - t; 
   }
   int z = -x - y;
   return sf::Vector3i(x, y, z);
