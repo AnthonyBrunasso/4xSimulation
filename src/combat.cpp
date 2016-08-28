bb8b2a4914a1490ff36592c9b6fb512de2f3c839
Alan Newton <alan@rufe.org>
2016-08-28 09:54:42 -0700

    Retune unit combat stats

--- src/combat.cpp
+++ src/combat.cpp
@@ -67,7 +67,13 @@ bool combat::calculate_modifiers(Unit* attacker, Unit* defender, Modifier& attac
   // Attacker and defender are facing *nearly* the same direction, this is a backstab.
   if (cmath::dot(attacker->m_direction, defender->m_direction) > 0) {
     std::cout << attacker->m_id << " backstabs " << defender->m_id << std::endl;
-    attacker_modifier.m_attack_modifier = 2.5f;
+    if (attacker->m_combat_stats.m_range <= 1) {
+      attacker_modifier.m_attack_modifier = 3.f;
+    }
+    else {
+      attacker_modifier.m_attack_modifier = 2.f;
+    }
+    
   }
 
   return true;
