4d214516ae1a3dfa1023c6a75a899c426c813c19
Alan Newton <alan@rufe.org>
2017-05-27 15:12:47 -0700

    binary_network_server.py now auto-exits
    * grace period allows for clients to reconnect
    * after grace period, exit if no clients reconnect

--- binary_network_server.py
+++ binary_network_server.py
@@ -3,20 +3,21 @@ import socket
 import struct
 
 binary_game_state = []
+server_in_use = asyncio.Event()
+connected_clients = 0
 
-@asyncio.coroutine
-def binary_reader(reader):
+async def binary_reader(reader):
   global binary_game_state
   while True:
     try:
-      header = yield from reader.readexactly(4)
+      header = await reader.readexactly(4)
       print('Header completed', header)
       header_size, = struct.unpack('I', header)
       print('Computed body size', header_size)
-      body = yield from reader.readexactly(header_size)
+      body = await reader.readexactly(header_size)
       print('Message read')
     except asyncio.streams.IncompleteReadError as e:
-      print('Client connection lost')
+      print('Stream read failed')
       return
     # Game message read
     message = bytearray()
@@ -24,42 +25,66 @@ def binary_reader(reader):
     message += body
     binary_game_state.append(message)
   
-@asyncio.coroutine
-def binary_writer(writer):
+async def binary_writer(writer):
   message_index = 0
   while True:
     print('writer tick')
-    yield from asyncio.sleep(1)
+    await asyncio.sleep(1)
     if len(binary_game_state) <= message_index:
       continue
     print('message queued')
     message = binary_game_state[message_index]
     message_index += 1
     writer.write(message)
-    yield from writer.drain()
+    await writer.drain()
     print('message sent', len(message), 'bytes')
-    
-@asyncio.coroutine
-def binary_game_client(reader, writer):
+
+async def handle_binary_game_client(reader, writer):
+  global server_in_use, connected_clients
+  server_in_use.set()
+  connected_clients += 1
+  print('New client connection')
   tasks = [
-     asyncio.async(binary_reader(reader)),
-     asyncio.async(binary_writer(writer)),
+     asyncio.ensure_future(binary_reader(reader)),
+     asyncio.ensure_future(binary_writer(writer)),
   ]
-  done, pending = yield from asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-  print('Connection read/write finalization')
+  done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
   for p in pending:
     p.cancel()
-  print('Connection read/write exiting')
+  connected_clients -= 1
+  print('Lost client connection')
   
-@asyncio.coroutine
-def acceptor():
-  server = yield from asyncio.start_server(binary_game_client, host='0.0.0.0', port=4000, backlog=8)
-  yield from server.wait_closed()
+async def auto_exit(server):
+  await server_in_use.wait()
+  print('a game began')
+
+  # Wait 12 cycles with no active players before exiting
+  def reset_cycle():
+    return 12
+  cycle = reset_cycle()
+  while cycle > 0:
+    await asyncio.sleep(5)
+    # While clients are connected, wait
+    if connected_clients > 0:
+      print('server has active clients')
+      cycle = reset_cycle()
+      continue
+    print('server has NO active clients')
+    cycle -=  1
+    
+async def acceptor(port):
+  server = await asyncio.start_server(handle_binary_game_client, host='0.0.0.0', port=port, backlog=8)
+  print('Server is listening on port', port)
+  tasks = [
+          asyncio.ensure_future(server.wait_closed()),
+          asyncio.ensure_future(auto_exit(server))
+  ]
+
+  done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+  for p in pending:
+    p.cancel()
   print('Exiting')
 
 if __name__ == "__main__":
   loop = asyncio.get_event_loop()
-  loop.run_until_complete(acceptor())
-
-  # Run the event loop
-  loop.run_forever()
+  loop.run_until_complete(acceptor(4000))
